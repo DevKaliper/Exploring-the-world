7bca3240a04c2f003f671c7e5ffbb3469f0820f0
Dev Kaliper <gxlthz@gmail.com>
2023-04-11 00:08:55 -0500

    p done

--- src/script.js
+++ src/script.js
@@ -1,16 +1,20 @@
 const closeMenu = document.getElementById("close")
 const menu = document.getElementById("menu")
 const openMenu = document.getElementById("menuOptions")
+const cities = document.getElementById("cities")
+
 
 menu.addEventListener("click", e =>{
     openMenu.classList.toggle("invisible")
     closeMenu.classList.toggle("hidden")
+    cities.classList.add("hidden")
 
 })
 
 closeMenu.addEventListener("click", e=>{
     openMenu.classList.toggle("invisible")
     closeMenu.classList.toggle("hidden")
+    cities.classList.remove("hidden")
 
 
 })
